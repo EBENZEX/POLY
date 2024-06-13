f5548abd3c2188dfa5b307b41326618c71fc6099
Athaya Naufal Brasta <147011661+athayanaufalbrasta@users.noreply.github.com>
2024-06-13 22:48:39 +0700

    add style

---
+++ src/css/style.js
@@ -0,0 +1,41 @@
+export const nav = {
+	link: "hover:underline decoration-[#836FFF] hover:scale-125",
+	linkSmall: "hover:bg-slate-200 pl-8 py-3 ",
+	box: "hidden sm:flex text-[#836FFF] text-lg font-semibold",
+};
+
+export const container = {
+	fluid: "w-screen px-10 md:px-20 py-4 md:py-8",
+	box1: "container py-8 mx-auto grid gap-10",
+	box2: "container mx-auto grid gap-10",
+};
+
+export const font = {
+	heading: "text-center font-bold text-5xl",
+};
+
+export const bussiness = {
+	cardContainer: "grid lg:grid-cols-3 gap-10 text-center",
+	card: "flex flex-col gap-5 text-white font-semibold text-2xl rounded-[1.5rem] bg-gradient-to-b from-[#836FFF] to-[#4F4399] py-10 px-16 justify-center items-center ",
+};
+
+export const form = {
+	input: "pl-2 transition-transform bg-inherit duration-100 ease-in-out border-b-2 border-slate-400 text-slate-600 font-bold border-slate-600 focus:outline-none",
+	box: "flex flex-col justify-center gap-10 bg-[#E2E8FF] py-10 px-20 md:h-fit",
+	btn: "text-xl rounded-2xl font-bold py-2 bg-[#15F5BA] hover:bg-[#13c293]",
+};
+
+export const footer = {
+	fluid: "text-center text-lg font-semibold -mb-20 text-white",
+	box: "bg-gradient-to-r from-[#836FFF] py-5 to-[#4F4399]",
+};
+
+export const dashboard = {
+	nav:`h-full w-min flex text-white font-bold text-lg flex-col px-4 justify-between py-10 bg-gradient-to-b from-[#836FFF] to-[#4F4399]`,
+	itemsContainer:"flex flex-col gap-5 items-center",
+	navItems: `flex items-center gap-3 p-2 border-b-2 border-white justify-between`,
+};
+
+export const carousel = {
+	card:"flex items-center justify-center h-full my-20 size-52"
+}
